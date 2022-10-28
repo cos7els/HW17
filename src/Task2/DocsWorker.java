a5f0e3de65027510dd1dd7520c53d17d5b409ae4
cos7els <cos7els@gmail.com>
2022-10-28 21:33:17 +0300

    Fix

--- src/Task2/DocsWorker.java
+++ src/Task2/DocsWorker.java
@@ -13,7 +13,7 @@ public class DocsWorker {
     private final Pattern DOC_NUMBER_PATTERN = Pattern.compile("^[0-9]{4}-[a-z]{3}-[0-9]{4}-[a-z]{3}-[0-9][a-z][0-9][a-z]");
     private final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("\\+\\d{3}\\(\\d{2}\\)\\d{7}");
     private final Pattern EMAIL_PATTERN = Pattern.compile("[0-9a-z]{4,12}@[a-z]{2,12}\\.[a-z]{2,3}");
-    private Map<String, Document> data = new HashMap<>();
+    private final Map<String, Document> data = new HashMap<>();
 
     public void start() {
         String read;
@@ -53,9 +53,11 @@ public class DocsWorker {
             System.out.println("Something wrong with read from file");
         }
         Document document = new Document();
-        strings.stream().map(DOC_NUMBER_PATTERN::matcher).forEach(x -> x.results().forEach(s -> document.addDocNumbers(s.group())));
-        strings.stream().map(PHONE_NUMBER_PATTERN::matcher).forEach(x -> x.results().forEach(s -> document.addPhoneNumbers(s.group())));
-        strings.stream().map(EMAIL_PATTERN::matcher).forEach(x -> x.results().forEach(s -> document.addeMails(s.group())));
+        for (String s : strings) {
+            DOC_NUMBER_PATTERN.matcher(s).results().forEach(x -> document.addDocNumbers(x.group()));
+            PHONE_NUMBER_PATTERN.matcher(s).results().forEach(x -> document.addPhoneNumbers(x.group()));
+            EMAIL_PATTERN.matcher(s).results().forEach(x -> document.addeMails(x.group()));
+        }
         if (document.getDocsSize() > 0) {
             data.put(file.getName().replace(".txt", ""), document);
         }
